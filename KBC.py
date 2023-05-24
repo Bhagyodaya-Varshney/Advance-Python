594c6c0a43e30951095e1dbeca0413d54d40f1b6
Bhagyodaya-Varshney <126950178+Bhagyodaya-Varshney@users.noreply.github.com>
2023-05-24 10:40:48 +0530

    KBC

---
+++ KBC.py
@@ -0,0 +1,199 @@
+import time
+def Ques1():
+    print("---------- First Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 1: Pongal is a popular festival of which state?''')
+    time.sleep(3)
+    print()
+    o1=['A: Karnataka','B: Kerala','C: Tamil Nadu','D: Andhra Pradesh']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'C' or ans1 == 'c'):
+        print('Congratulations You Have Won 1000KBC RS.')
+        Ques2()
+    else:
+        print('Oh No You Answered Worng')
+        print('You Won No Rewards')
+        exit()
+def Ques2():
+    time.sleep(5)
+    print("---------- Second Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 2: Ghototkach in Mahabharat was the son of''')
+    time.sleep(3)
+    print()
+    o1=['A: Arjun','B: Bhim','C: Duryodhan','D: Nakul']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'B' or ans1 == 'b'):
+        print('Congratulations You Have Won 2000 KBC RS.')
+        Ques3()
+    else:
+        print('Oh No You Answered Worng')
+        print("Don't Worry You Won 1000KBC Rs")
+        exit()
+def Ques3():
+    time.sleep(5)
+    print("---------- Third Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 3: The first month of the Indian national calendar is''')
+    time.sleep(3)
+    print()
+    o1=['A: Magha','B: Chaitra','C: Ashadha','D: Vaishakha']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'B' or ans1 == 'b'):
+        print('Congratulations You Have Won 3000 KBC RS.')
+        Ques4()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 2000KBC Rs")
+        exit()
+def Ques4():
+    time.sleep(5)
+    print("---------- Forth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 4: Who holds a national record with a timing of 50.79 seconds in women's 400m track events?''')
+    time.sleep(3)
+    print()
+    o1=['A: Sophie Essclestone','B: Sania Mirza','C: Sunita Rani','D: Hima Das']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'D' or ans1 == 'd'):
+        print('Congratulations You Have Won 4000 KBC RS.')
+        Ques5()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 3000KBC Rs")
+        exit()
+def Ques5():
+    time.sleep(5)
+    print("---------- Fivth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 5: Which cricket ground is popularly called Gabba?''')
+    time.sleep(3)
+    print()
+    o1=['A: Wankhade Cricket Ground','B: Melbourn Cricket Ground','C: Brisbane Cricket Ground','D: Sydney Cricket Ground']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'C' or ans1 == 'c'):
+        print('Congratulations You Have Won 5000 KBC RS.')
+        Ques6()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 4000KBC Rs")
+        exit()
+def Ques6():
+    time.sleep(5)
+    print("---------- Sixth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 6: The weight of a Heart of a normal adult human being is?''')
+    time.sleep(3)
+    print()
+    o1=['A: 300 Grams','B: 250 Grams','C: 150 Grams','D: 200 Grams']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'A' or ans1 == 'a'):
+        print('Congratulations You Have Won 6000 KBC RS.')
+        Ques7()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 5000KBC Rs")
+        exit()
+def Ques7():
+    time.sleep(5)
+    print("---------- Seventh Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 7: Largest Muslim country in Asia as well as in World is called''')
+    time.sleep(3)
+    print()
+    o1=['A: Pakistan','B: bangladesh','C: Indonesia','D: Iran']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'C' or ans1 == 'c'):
+        print('Congratulations You Have Won 7000 KBC RS.')
+        Ques8()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 6000KBC Rs")
+        exit()
+def Ques8():
+    time.sleep(5)
+    print("---------- Eigth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 8: Which of these scientists does not have a chemical element on the periodic table named after him?''')
+    time.sleep(3)
+    print()
+    o1=['A: Thomas Edison','B: Albert Einstein','C: Enric Fermi','D: Alfred Nobel']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'A' or ans1 == 'a'):
+        print('Congratulations You Have Won 8000 KBC RS.')
+        Ques9()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 7000KBC Rs")
+        exit()
+def Ques9():
+    time.sleep(5)
+    print("---------- Ninth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 9: Against Which team did Rohit Sharma take the only hat-trick in IPL''')
+    time.sleep(3)
+    print()
+    o1=['A: Mumbai Indian','B: Deccan Chargers','C: Royal Challengers Banglore','D: Gujrat Titans']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'B' or ans1 == 'b'):
+        print('Congratulations You Have Won 9000 KBC RS.')
+        Ques10()
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 8000KBC Rs")
+        exit()
+def Ques10():
+    time.sleep(5)
+    print("---------- Tenth Question On Your Computer Screen ----------")
+    time.sleep(3)
+    print('''Question 10: Which is the First African Country to Elect a Woman President''')
+    time.sleep(3)
+    print()
+    o1=['A: Liberian','B: Ghana','C: Namibia','D: South Africa']
+    for i in o1:
+        print(i)
+    print()
+    ans1=input("Enter The Answer:")
+    if(ans1 == 'A' or ans1 == 'a'):
+        print('Congratulations You Have Won 10000 KBC RS.')
+        print('You Answered All Questions! You Are genious')
+    else:
+        print('Oh No You Answered Worng! Try Again')
+        print("Don't Worry You Won 9000KBC Rs")
+        exit()
+print('''Games Rules:
+      There are total 10 Questions.
+      For each Question You Got Rewareded with KBC Currency.
+      For each Question You have 4 Options to Ans.
+      There is No LifeLine.''')
+print("Type 'START' for Starting the Game")
+a=input()
+if(a =='START' or a == 'start'):
+    Ques1()   
